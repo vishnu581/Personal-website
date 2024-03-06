5f35ae92a0521965b2e9cc28be5d0f61c8bd9d20
vishnu581 <vishnuvenugopal581@gmail.com>
2024-03-06 12:46:57 +0530

    from submission added

--- js/main.js
+++ js/main.js
@@ -77,3 +77,14 @@
     });
 })(jQuery);
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzFxmva823X3IbQXAIQM-3yGLXh309lIb02mTBdpnb7c9eMYBZzJwP_y7TYmjHocIqz/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
